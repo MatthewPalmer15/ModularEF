9bfac6a2ebf796d07ce1741da5880797c0af8abe
Matthew Palmer <78506314+MatthewPalmer15@users.noreply.github.com>
2023-10-24 22:53:05 +0100

    Updated factories

--- Modular/Services/Factories/Entity/OrganisationFactory.cs
+++ Modular/Services/Factories/Entity/OrganisationFactory.cs
@@ -20,7 +20,109 @@ namespace Modular.Core.Models.Entity
         {
             modelBuilder.Entity<Organisation>(entity =>
             {
-                entity.ToTable("tbl_Organisation");
+                entity.ToTable("tblOrganisation");
+
+                entity.HasKey(e => e.ID)
+                      .IsClustered(false);
+
+                entity.Property(e => e.ID)
+                      .HasColumnName("ID")
+                      .ValueGeneratedNever()
+                      .IsRequired();
+
+
+                entity.Property(x => x.CreatedDate)
+                      .HasColumnName("CreatedDate")
+                      //.ValueGeneratedOnAdd()
+                      .IsRequired();
+
+                entity.Property(x => x.CreatedBy)
+                      .HasColumnName("CreatedBy")
+                      //.ValueGeneratedOnAdd()
+                      .IsRequired();
+
+                entity.Property(x => x.ModifiedDate)
+                      .HasColumnName("ModifiedDate")
+                      //.ValueGeneratedOnUpdate()
+                      .IsRequired();
+
+                entity.Property(x => x.ModifiedBy)
+                      .HasColumnName("ModifiedBy")
+                      //.ValueGeneratedOnUpdate()
+                      .IsRequired();
+
+                entity.Property(x => x.Name)
+                      .HasColumnName("Name")
+                      .IsRequired()
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.Description)
+                      .HasColumnName("Description")
+                      .HasMaxLength(256);
+
+                entity.Property(x => x.RegistrationNumber)
+                      .HasColumnName("RegistrationNumber")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.OwnerID)
+                      .HasColumnName("OwnerID")
+                      .IsRequired();
+
+                entity.HasOne(x => x.Owner)
+                      .WithOne()
+                      .HasForeignKey<Organisation>(x => x.OwnerID);
+
+                entity.Property(x => x.AddressLine1)
+                      .HasColumnName("AddressLine1")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.AddressLine2)
+                      .HasColumnName("AddressLine2")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.AddressLine3)
+                      .HasColumnName("AddressLine3")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.AddressCity)
+                      .HasColumnName("AddressCity")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.AddressCounty)
+                      .HasColumnName("AddressCounty")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.AddressCountryID)
+                      .HasColumnName("AddressCountryID");
+
+                entity.HasOne(x => x.AddressCountry)
+                      .WithOne()
+                      .HasForeignKey<Organisation>(x => x.AddressCountryID);
+
+                entity.HasIndex(e => e.AddressCountryID)
+                      .IsUnique();
+
+                entity.Property(x => x.AddressPostcode)
+                      .HasColumnName("AddressPostcode")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.Email)
+                      .HasColumnName("Email")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.Telephone)
+                      .HasColumnName("Telephone")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.Website)
+                      .HasColumnName("Website")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.Status)
+                      .HasColumnName("Status")
+                      .HasColumnType("int")
+                      .IsRequired();
+
             });
 
         }
--- Modular/Services/Factories/Identity/IdentityFactory.cs
+++ Modular/Services/Factories/Identity/IdentityFactory.cs
@@ -20,23 +20,90 @@ namespace Modular.Core.Models.Entity
         public static void OnModelCreating(ModelBuilder modelBuilder)
         {
 
-            modelBuilder.Entity<ApplicationUser>()
-            .ToTable("ApplicationUser")
-            .HasKey(x => x.Id);
+            modelBuilder.Entity<ApplicationUser>(entity =>
+            {
+                entity.ToTable("tblApplicationUsers");
 
-            modelBuilder.Entity<ApplicationUser>()
-                .HasOne(x => x.Contact)
-                .WithOne()
-                .HasForeignKey<ApplicationUser>(x => x.ContactID);
+                entity.HasKey(e => e.Id)
+                      .IsClustered(false);
 
 
-            modelBuilder.Entity<ApplicationUser>()
-                .Property(x => x.ContactID)
-                .ValueGeneratedOnAdd();
+                entity.Property(e => e.Id)
+                      .HasColumnName("ID")
+                      .ValueGeneratedNever()
+                      .IsRequired();    
 
-            modelBuilder.Entity<ApplicationUser>()
-                .HasIndex(e => e.ContactID)
-                .IsUnique();
+
+                entity.Property(x => x.UserName)
+                      .HasColumnName("Username")
+                      .IsRequired()
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.NormalizedUserName)
+                      .HasColumnName("NormalizedUsername")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.Email)
+                      .HasColumnName("Email")
+                      .IsRequired()
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.NormalizedEmail)
+                      .HasColumnName("NormalizedEmail")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.PhoneNumber)
+                      .HasColumnName("PhoneNumber")
+                      .HasMaxLength(128);
+
+                entity.Property(x => x.PasswordHash)
+                      .HasColumnName("PasswordHash")
+                      .IsRequired()
+                      .HasMaxLength(128);                
+                
+                entity.Property(x => x.EmailConfirmed)
+                      .HasColumnName("EmailConfirmed");
+
+                entity.Property(x => x.PhoneNumberConfirmed)
+                      .HasColumnName("PhoneNumberConfirmed");
+
+                entity.Property(x => x.TwoFactorEnabled)
+                      .HasColumnName("TwoFactorEnabled");
+
+                entity.Property(x => x.LockoutEnabled)
+                      .HasColumnName("LockoutEnabled");
+
+                entity.Property(x => x.LockoutEnd)
+                      .HasColumnName("LockoutEnd");
+
+                entity.Property(x => x.AccessFailedCount)
+                      .HasColumnName("AccessFailedCount");
+
+                entity.Property(x => x.SecurityStamp)
+                      .HasColumnName("SecurityStamp");
+
+                entity.Property(x => x.ConcurrencyStamp)
+                      .HasColumnName("ConcurrencyStamp");
+
+                entity.Property(x => x.ContactID)
+                    .HasColumnName("ContactID")
+                    .ValueGeneratedOnAdd();
+
+                entity.HasOne(x => x.Contact)
+                      .WithOne()
+                      .HasForeignKey<ApplicationUser>(x => x.ContactID);
+
+                entity.HasIndex(e => e.ContactID)
+                      .IsUnique();
+
+                entity.Property(x => x.IsStaff)
+                      .HasColumnName("IsStaff");
+
+                entity.Property(x => x.IsAdmin)
+                      .HasColumnName("IsAdmin");
+
+
+            });
 
         }
     }
