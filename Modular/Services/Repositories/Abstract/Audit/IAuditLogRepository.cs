61a1bf855849d0248970bd5eebae100898ff2cfd
Matthew Palmer <78506314+MatthewPalmer15@users.noreply.github.com>
2023-10-25 17:24:48 +0100

    Added Abstract interfaces

---
+++ Modular/Services/Repositories/Abstract/Audit/IAuditLogRepository.cs
@@ -0,0 +1,14 @@
+﻿using Modular.Core.Interfaces;
+using Modular.Core.Models.Audit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modular.Core.Services.Repositories.Abstract.Audit
+{
+    public interface IAuditLogRepository : IRepository<AuditLog>, IDisposable
+    {
+    }
+}
---
+++ Modular/Services/Repositories/Abstract/Misc/IOccupationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Modular.Core.Interfaces;
+using Modular.Core.Models.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modular.Core.Services.Repositories.Abstract.Misc
+{
+    public interface IOccupationRepository : IRepository<Occupation>, IDisposable
+    {
+    }
+}
