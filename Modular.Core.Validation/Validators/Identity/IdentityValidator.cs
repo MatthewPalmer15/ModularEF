54aef21d472b58deb891440aa557eb6ac939c74c
Matthew Palmer <78506314+MatthewPalmer15@users.noreply.github.com>
2023-11-14 22:42:07 +0000

    Updated Validation

---
+++ Modular.Core.Validation/Validators/Identity/IdentityValidator.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation;
+using Modular.Core.Identity;
+using Modular.Core.Models.Config;
+using Modular.Core.Models.Location;
+using System.Text.RegularExpressions;
+
+namespace Modular.Core.Validaton.Location
+{
+    public class IdentityValidator : AbstractValidator<ApplicationUser>
+    {
+
+        private readonly ModularIdentityDbContext _context;
+
+        public IdentityValidator(ModularIdentityDbContext context)
+        {
+            _context = context;
+
+            RuleFor(e => e.UserName)
+                .NotEmpty().WithMessage("Name is required")
+                .Must(IsUserNameUnique).WithMessage("Username is already used. Please enter a unique username.)");
+
+            RuleFor(e => e.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .Must(IsEmailUnique).WithMessage("Email is already used. Please enter another email.)");
+
+            RuleFor(e => e.PasswordHash)
+                .NotEmpty().WithMessage("Password is required");
+
+            RuleFor(e => e.PhoneNumber)
+                .MaximumLength(20).WithMessage("Phone Number must not exceed 50 characters.")
+                .Matches(new Regex(@"((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}")).WithMessage("Phone Number not valid");
+
+            RuleFor(e => e.ContactId)
+                .NotEmpty().WithMessage("Contact is required");
+
+        }
+
+        private bool IsUserNameUnique(string? userName)
+        {
+            ApplicationUser? applicationUser = _context.ApplicationUsers
+                    .Where(x => x.UserName != null && x.UserName.Trim().ToUpper() == userName.Trim().ToUpper())
+                    .SingleOrDefault();
+
+            return applicationUser == null;
+        }
+
+        private bool IsEmailUnique(string? email)
+        {
+            ApplicationUser? applicationUser = _context.ApplicationUsers
+                    .Where(x => x.Email != null && x.Email.Trim().ToUpper() == email.Trim().ToUpper())
+                    .SingleOrDefault();
+
+            return applicationUser == null;
+        }
+
+
+    }
+}
---
+++ Modular.Core.Validation/Validators/Misc/OccupationValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Modular.Core.Models.Misc;
+
+namespace Modular.Core.Validaton.Misc
+{
+    public class OccupationValidator : AbstractValidator<Occupation>
+    {
+
+        public OccupationValidator()
+        {
+
+            RuleFor(e => e.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .MaximumLength(128).WithMessage("Name must not exceed 128 characters.");
+
+
+            RuleFor(e => e.Description)
+                .MaximumLength(512).WithMessage("Description must not exceed 512 characters.");
+
+        }
+
+    }
+}
