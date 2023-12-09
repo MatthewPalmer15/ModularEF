76eb9f1fbc2fbc03bc008f6d731de77b244b088a
Matthew Palmer <78506314+MatthewPalmer15@users.noreply.github.com>
2023-12-09 13:13:26 +0000

    Upgraded User Manager

--- Modular.Core.Services/Concrete/ModularUserManager.cs
+++ Modular.Core.Services/Concrete/ModularUserManager.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Modular.Core.Entities;
 using Modular.Core.Identity;
+using System.Security.Claims;
 
 namespace Modular.Core.Services.Identity
 {
@@ -18,6 +19,8 @@ namespace Modular.Core.Services.Identity
             UserManager = userManager;
             RoleManager = roleManager;
         }
+
+        #region "  Users  "
 
         /// <summary>
         /// Creates a new application user. Returns null if it was not successful.
@@ -43,38 +46,174 @@ namespace Modular.Core.Services.Identity
                 return IdentityResult.Failed(createUserResult.Errors.ToArray());
             }
 
-            foreach (string role in roles)
+            var createRolesResult = await AssignRolesAsync(applicationUser, createIfNull, roles);
+            if (!createRolesResult.Succeeded)
             {
-                var createRoleResult = await applicationUser.AssignRoleAsync(UserManager, RoleManager, role, createIfNull);
-                if (!createRoleResult.Succeeded)
-                {
-                    return IdentityResult.Failed(createRoleResult.Errors.ToArray());
-                }
+                return IdentityResult.Failed(createRolesResult.Errors.ToArray());
             }
-
             return IdentityResult.Success;
         }
 
+        #endregion
+
+        #region "  Roles  "
+
+        /// <summary>
+        /// Adds role(s) to a user.
+        /// </summary>
+        /// <param name="applicationUser"></param>
+        /// <param name="createIfNull"></param>
+        /// <param name="roles"></param>
+        /// <returns></returns>
         public async Task<IdentityResult> AssignRolesAsync(ApplicationUser applicationUser, bool createIfNull, params string[] roles)
         {
             foreach (string role in roles)
             {
-                var result = await applicationUser.AssignRoleAsync(UserManager, RoleManager, role, createIfNull);
-                if (!result.Succeeded) 
+                //  Get role, and if it does not exist, create it if allowed.
+                bool roleExists = await RoleManager.RoleExistsAsync(role);
+                if (!roleExists)
                 {
-                    return IdentityResult.Failed(result.Errors.ToArray());
+                    if (createIfNull)
+                    {
+                        ApplicationRole newApplicationRole = new ApplicationRole { Name = role };
+                        var createRoleResult = await RoleManager.CreateAsync(newApplicationRole);
+                        if (!createRoleResult.Succeeded)
+                        {
+                            return IdentityResult.Failed(createRoleResult.Errors.ToArray());
+                        }
+                    }
+                    else
+                    {
+                        return IdentityResult.Failed(new IdentityError() { Code = "RoleNotFound", Description = "Role was not found, and could not be created." });
+                    }
+                }
+
+                //  Assign role to user
+                var addRoleResult = await UserManager.AddToRoleAsync(applicationUser, role);
+                if (!addRoleResult.Succeeded)
+                {
+                    return IdentityResult.Failed(addRoleResult.Errors.ToArray());
+                }
+
+
+            }
+
+            return IdentityResult.Success;
+        }
+
+        /// <summary>
+        /// Removes role(s) from a user.
+        /// </summary>
+        /// <param name="applicationUser"></param>
+        /// <param name="roles"></param>
+        /// <returns></returns>
+        public async Task<IdentityResult> RevokeRolesAsync(ApplicationUser applicationUser, params string[] roles)
+        {
+            foreach (string role in roles)
+            {
+                bool roleExists = await RoleManager.RoleExistsAsync(role);
+                if (roleExists)
+                {
+                    var removeRoleResult = await UserManager.RemoveFromRoleAsync(applicationUser, role);
+                    if (!removeRoleResult.Succeeded)
+                    {
+                        return IdentityResult.Failed(removeRoleResult.Errors.ToArray());
+                    }
                 }
             }
 
             return IdentityResult.Success;
         }
 
-        //public string GenerateApiToken()
-        //{
-        //    Guid UUID = Guid.NewGuid();
-        //    string apiToken = UUID.ToString().Replace("-", "");
-        //    return UUID.ToString();
-        //}
+
+        #endregion
+
+        #region "  Claims  "
+
+        /// <summary>
+        /// Adds Claim(s) to a user.
+        /// </summary>
+        /// <param name="applicationUser"></param>
+        /// <param name="claims"></param>
+        /// <returns></returns>
+        public async Task<IdentityResult> AssignClaimsAsync(ApplicationUser applicationUser, params Claim[] claims)
+        {
+            IList<Claim> userClaims = await UserManager.GetClaimsAsync(applicationUser);
+
+            foreach (Claim claim in claims)
+            {
+                Claim? existingClaim = userClaims.FirstOrDefault(x => x.Type == claim.Type);
+
+                if (existingClaim != null)
+                {
+                    var replaceClaimResult = await UserManager.ReplaceClaimAsync(applicationUser, existingClaim, claim);
+                    if (!replaceClaimResult.Succeeded)
+                    {
+                        return IdentityResult.Failed(replaceClaimResult.Errors.ToArray());
+                    }
+
+                }
+                else
+                {
+                    var addClaimResult = await UserManager.AddClaimAsync(applicationUser, claim);
+                    if (!addClaimResult.Succeeded)
+                    {
+                        return IdentityResult.Failed(addClaimResult.Errors.ToArray());
+                    }
+
+                }
+            }
+
+            return IdentityResult.Success;
+        }
+
+
+        /// <summary>
+        /// Removes Claim(s) from a user.
+        /// </summary>
+        /// <param name="applicationUser"></param>
+        /// <param name="claims"></param>
+        /// <returns></returns>
+        public async Task<IdentityResult> RevokeClaimsAsync(ApplicationUser applicationUser, params Claim[] claims)
+        {
+            IList<Claim> userClaims = await UserManager.GetClaimsAsync(applicationUser);
+            foreach (Claim claim in claims)
+            {
+                Claim? existingClaim = userClaims.FirstOrDefault(x => x.Type == claim.Type);
+
+                if (existingClaim != null)
+                {
+                    var replaceClaimResult = await UserManager.RemoveClaimAsync(applicationUser, existingClaim);
+                    if (!replaceClaimResult.Succeeded)
+                    {
+                        return IdentityResult.Failed(replaceClaimResult.Errors.ToArray());
+                    }
+
+                }
+            }
+
+            return IdentityResult.Success;
+        }
+
+        #endregion
+
+        #region "  Tokens  "
+
+        public async Task<IdentityResult> AssignTokenAsync(ApplicationUser applicationUser, string loginProvider, string tokenName, string? tokenValue)
+        {
+            var result = await UserManager.SetAuthenticationTokenAsync(applicationUser, loginProvider, tokenName, tokenValue);
+            return result;
+        }
+
+        public async Task<IdentityResult> RevokeTokenAsync(ApplicationUser applicationUser, string loginProvider, string tokenName)
+        {
+            var result = await UserManager.RemoveAuthenticationTokenAsync(applicationUser, loginProvider, tokenName);
+            return result;
+        }
+
+        #endregion
 
     }
+
+
 }
