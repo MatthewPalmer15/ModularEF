77fd816ad8dfe58848183c62f19d7e807cb64237
Matthew Palmer <78506314+MatthewPalmer15@users.noreply.github.com>
2023-12-13 23:38:02 +0000

    Updated repo

--- Modular.Core.Services/Services/Repositories/Abstract/Configuration/IConfigurationRepository.cs
+++ Modular.Core.Services/Services/Repositories/Abstract/Configuration/IConfigurationRepository.cs
@@ -5,7 +5,7 @@ namespace Modular.Core.Services.Repositories.Abstract
 {
     public interface IConfigurationRepository : IRepository<Configuration>, IDisposable
     {
-        string? GetValue(string key);
+        public string? GetValue(string key);
 
     }
 }
