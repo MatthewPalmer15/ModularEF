9604b129efb990fe974d566a947b57e6f4950ec3
Matthew Palmer <78506314+MatthewPalmer15@users.noreply.github.com>
2023-12-20 17:01:37 +0000

    Added API project.

---
+++ Modular.Core.Api/Services/Abstract/ApiService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Headers;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Modular.Core.Api
+{
+
+    public class ApiService : IApiService
+    {
+
+        private readonly HttpClient _httpClient;
+
+        public ApiService(IConfiguration configuration) 
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(configuration["API_BASE_URL"] ?? "");
+            _httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+        public async Task<string> GetAsync(string url, Guid id)
+        {
+            var response = await _httpClient.GetAsync($"{url}/{id.ToString()}");
+            if (response.IsSuccessStatusCode)
+            {
+                var objects = response.Content.ReadAsStringAsync().Result;
+                return objects;
+            }
+            else
+            {
+                return string.Empty;
+            }
+
+        }
+
+
+    }
+
+}
---
+++ Modular.Core.Api/Services/Concrete/IApiService.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Headers;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Modular.Core.Api
+{
+
+    public interface IApiService
+    {
+
+        public Task<string> GetAsync(string url, Guid id);
+
+    }
+
+}
