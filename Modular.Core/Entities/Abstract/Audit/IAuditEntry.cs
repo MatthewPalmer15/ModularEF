e5528853d573258bddae5b6e08e52f638ca5d70e
Matthew Palmer <78506314+MatthewPalmer15@users.noreply.github.com>
2023-12-21 17:21:54 +0000

    Added some abstraction classes (Maybe eventually allow people to build their own on top?)

---
+++ Modular.Core/Entities/Abstract/Audit/IAuditEntry.cs
@@ -0,0 +1,36 @@
+﻿#nullable disable
+
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Modular.Core.Interfaces;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Modular.Core.Entities
+{
+    public interface IAuditEntry : IBaseEntity<Guid>
+    {
+
+        public enum ActionType
+        {
+            Unknown,
+            Create,
+            Update,
+            Delete
+        }
+
+        public DateTime Timestamp { get; set; }
+
+        public Guid ApplicationUserId { get; set; }
+
+        public ActionType Action { get; set; }
+
+        public string EntityId { get; set; }
+
+        public string EntityName { get; set; }
+
+        public Dictionary<string, object> EntityObject { get; set; }
+
+        [NotMapped]
+        public List<PropertyEntry> TempProperties { get; set; }
+
+    }
+}
